28de9563e3242b95805eb66b851ad873bc528ac5
freakboy791 <45658876+freakboy791@users.noreply.github.com>
2025-08-23 19:46:50 +0000

    fix confirm email token issue

--- app/auth/callback/hello.tsx
+++
@@ -1,2 +0,0 @@
-// app/auth/callback/hello.tsx
-export default function Hello() { return <h1>Hi there</h1> }
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   // ...other config options
 };
 
